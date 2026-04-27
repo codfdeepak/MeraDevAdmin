6ed745f9f538124234301d6e5a576267fe4c18c3
DotcomDeveloper <dotcomgadgets@gmail.com>
2026-04-27 18:34:37 +0530

    update for live

--- src/store/thunks/heroThunks.js
+++ src/store/thunks/heroThunks.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
-
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5500'
+import { API_URL } from '../../config/api'
 
 const authedRequest = async (path, { token, ...options }) => {
   const res = await fetch(`${API_URL}${path}`, {
